d3b1633380a7160a2260e60267d2a0513fda88ff
Sanchit Patil <Sanchit.Patil@wissen.com>
2025-02-13 16:24:29 +0530

    Added Test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest{
+
+    @Test
+    public void testAdd() {
+        
+    }
+
+    @Test
+    public void testSub() {
+        
+    }
+}
