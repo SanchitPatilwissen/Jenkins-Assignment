81b140e3ef1e9d14fe7b71643f45f7379fb019f7
Sanchit Patil <Sanchit.Patil@wissen.com>
2025-02-13 16:34:34 +0530

    Added Test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -13,7 +13,7 @@ public class CalculatorTest{
     @Test
     public void testSub() {
     	Calculator c1 = new Calculator();
-        assertEquals(-10, c1.add(10, 20));
+        assertEquals(-10, c1.sub(10, 20));
     }
     
     @Test
